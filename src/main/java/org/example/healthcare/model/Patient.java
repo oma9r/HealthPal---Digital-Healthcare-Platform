67d8827ae5e7cbdad9289868e682d1b1a21a79a7
saif-shayeb <sshayep@gmail.com>
2025-10-27 13:20:16 +0200

    editing Patient model

--- src/main/java/org/example/healthcare/model/Patient.java
+++ src/main/java/org/example/healthcare/model/Patient.java
@@ -1,4 +1,15 @@
 package org.example.healthcare.model;
 
+import jakarta.persistence.*;
+
+@Table(name = "patient")
 public class Patient {
+    @Id
+    @Column(name = "patient_id")
+    private int patientId;
+    @OneToOne(cascade = CascadeType.ALL,orphanRemoval = true)
+    @JoinColumn(name = "user_Id",referencedColumnName = "userId",foreignKey = @ForeignKey(name = "fk_patient_user", foreignKeyDefinition = "FOREIGN KEY (user_id) REFERENCES users(userId) ON DELETE CASCADE"))
+    private User user;
+
+
 }
